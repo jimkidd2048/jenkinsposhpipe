d6a3bd5c07da0c9f4c74c47d7ede5eb536240e2f
jimk2048 <jimk2048@users.noreply.github.com>
2019-07-09 23:03:54 -0400

    update pester update command

---
+++ pester.ps1
@@ -0,0 +1,5 @@
+# upgrade Pester Module
+# https://github.com/pester/Pester/wiki/Installation-and-Update
+# Powershell has to run as administrator
+
+Install-Module -Name Pester -Force -SkipPublisherCheck
