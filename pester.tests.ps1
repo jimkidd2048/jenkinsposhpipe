c8c0e27924ec0935555a6843272a812ba6348131
jimk2048 <jimk2048@users.noreply.github.com>
2019-07-09 23:28:01 -0400

    update pester version test

--- pester.tests.ps1
+++ pester.tests.ps1
@@ -3,11 +3,4 @@ Describe "pester module tests" {
     It "Pester Module's Major Version is 4 or greater" {
         (get-module pester).version.major | Should -BeGreaterOrEqual 4
     }
-    It "Pester Module's Minor Version is 4 or greater" {
-        (get-module pester).version.minor | Should -BeGreaterOrEqual 4
-    }
-    It "Pester Module's Build Version is 2 or greater" {
-        (get-module pester).version.build | Should -BeGreaterOrEqual 2
-    }
-
 }
