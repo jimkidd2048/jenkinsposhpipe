b38d2d7e3fa37fa1f75f82efebe190f512494161
jimk2048 <jimk2048@users.noreply.github.com>
2019-07-09 22:35:33 -0400

    jenkins.bat

--- jenkins.bat
+++ jenkins.bat
@@ -1,1 +1,1 @@
-powershell.exe -command "Invoke-Pester -OutputFormat NUnitXml -OutputFile nunit.xml -Show None"
+powershell.exe -command "Invoke-Pester -OutputFormat NUnitXml -OutputFile .\nunit.xml"
